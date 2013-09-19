5914ab86e1b856dde50755d76bd4e3812d915408
Tim Uruski <github@login.timuruski.name>
2013-09-19 07:41:08 -0600

    Simple key extraction parser

--- lib/eliot/key_parser.rb
+++ lib/eliot/key_parser.rb
@@ -1,11 +1,17 @@
 module Eliot
   class KeyParser
-    def initialize(*args)
-
+    def initialize(keys, options = {})
+      @keys = keys
+      @separator = options.fetch(:separator, ':')
     end
 
     def parse
-      []
+      @keys.map { |pair|
+        raw_key, key = pair.split(@separator, 2)
+        key = raw_key if key.nil? || key == ''
+
+        [raw_key, key]
+      }
     end
   end
 end
--- spec/lib/eliot/key_parser_spec.rb
+++ spec/lib/eliot/key_parser_spec.rb
@@ -3,11 +3,43 @@ require 'eliot/key_parser'
 
 module Eliot
   describe KeyParser do
-    context "with a string of keys" do
+    context "with an array of paired values" do
       it "parses them into pairs of keys" do
-        subject = KeyParser.new('ID:id Email:email')
+        subject = KeyParser.new(%w[ID:id Email:email])
+
         expected_keys = [ ['ID', 'id'],
                           ['Email', 'email'] ]
+
+        parsed_keys = subject.parse
+        expect(parsed_keys).to eq(expected_keys)
+      end
+
+      it "parses single keys into a pair" do
+        subject = KeyParser.new(%w[id email])
+
+        expected_keys = [ ['id', 'id'],
+                          ['email', 'email'] ]
+
+        parsed_keys = subject.parse
+        expect(parsed_keys).to eq(expected_keys)
+      end
+
+      it "parses incomplete keys into a pair" do
+        subject = KeyParser.new(%w[id: email:])
+
+        expected_keys = [ ['id', 'id'],
+                          ['email', 'email'] ]
+
+        parsed_keys = subject.parse
+        expect(parsed_keys).to eq(expected_keys)
+      end
+
+      it "can use an arbitrary separator" do
+        subject = KeyParser.new(%w[user:id=id user:email=email],
+                                separator: '=')
+
+        expected_keys = [ ['user:id', 'id'],
+                          ['user:email', 'email'] ]
 
         parsed_keys = subject.parse
         expect(parsed_keys).to eq(expected_keys)
